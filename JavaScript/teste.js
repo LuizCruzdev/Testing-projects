84ac872236d2211df3ed3da24f765261c94d0656
Luiz Cruz <luiz96cruz@gmail.com>
2021-09-29 14:24:45 -0300

    third

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,8 +1,22 @@
 console.log('testando')
 
-const idade = 26
+// const idade = 26
 
-console.log(idade)
+// console.log(idade)
 
-
+// ctrl k c = comenta
+// ctrl k u = descomenta
 // second commit
+// third commmmmmit
+// Alt shift F will ident the code, which is useful to see if there is any { or } left
+const listaDeDestinos = new Array(
+        'Teste',
+        'teste2',
+        'teste3',
+    )
+    // if algo OU algo escreve-se: if algo ||(ou) algo ---- if algo E algo = if algo &&(E) algo
+console.log(listaDeDestinos)
+listaDeDestinos.push('teste4')
+console.log(listaDeDestinos)
+console.log(listaDeDestinos)
+console.log(listaDeDestinos)
