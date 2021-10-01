35cf151fee03038756489c1bc8e2a8263150ff9c
Luiz Cruz <luiz96cruz@gmail.com>
2021-10-01 10:03:37 -0300

    vue weather

--- weather-app/README.md
+++ weather-app/README.md
@@ -1,4 +1,4 @@
-# first-project
+# weather-app
 
 ## Project setup
 ```
---
+++ weather-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <main>
+        <div class='search-box'>
+          <input type='text' class='search-bar' placeholder='Search...'>
+
+        </div>
+      </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: '50763ceb6b5cb0cdfcbc97ec36ff0f08'
+    }
+
+  }
+}
+
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+ body {
+   font-family:'montserrat', sans-serif;
+  
+ }
+
+ #app {
+   background-image: url('.assets/cold-bg.jpg');
+ }
+
+</style>
