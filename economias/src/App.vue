835fba58671ae97b41bffda3f5e972b073e68c1b
Luiz Cruz <luiz96cruz@gmail.com>
2021-11-04 21:04:04 -0300

    Novo projeto de economias

--- economias/src/App.vue
+++ economias/src/App.vue
@@ -1,60 +1,30 @@
 <template>
-<div class='container'>
-  <header>
-    <h1>Planilha da Família Baleiorca Suprema!</h1>
-  </header>
-
-  <body>
-    <b-form>
-      <b-row>
-        <b-col>
-          <label>Meio de pagamento</label>
-          <select class="meio" v-model="nubank" id="inputGroupSelect01" >
-            <option selected>Choose...</option>
-            <option value="1">Dinheiro</option>
-            <option value="2" class='nubank'>Nubank</option>
-            <option value="3">BB</option>
-            <option value="4">VR</option>
-          </select>
-        </b-col>
-        <b-col>
-          <div class ='pag' v-if='nubank == true' :display=true>
-          <label>Forma de Pagamento de pagamento</label>
-            <select class="custom-select" id="inputGroupSelect01">
-              <option selected>Choose...</option>
-              <option value="1">Débito</option>
-              <option value="2">Crédito</option>
-            </select>
-          </div>
-        </b-col>
-      </b-row>
-    </b-form>
-  </body>
-
-</div>
-
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-
-<script>
-
-// data() {
-//     return {
-//         option: null
-//     }
-// }
-// var vm = new Vue({
-//     data: {
-//         key: ""
-//     },
-//     methods: {
-//         onChange(event) {
-//             console.log(event.target.value)
-//         }
-//     }
-// })
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
