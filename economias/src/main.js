ad500d127e81dba077f216d6cafc17349da2c624
Luiz Cruz <luiz96cruz@gmail.com>
2022-01-06 14:40:46 -0300

    save

--- economias/src/main.js
+++ economias/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).mount('#app');
--- economias/src/router/index.js
+++ economias/src/router/index.js
@@ -1,25 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+const routes = [{
+        path: '/',
+        name: 'Create New Expense',
+        component: () =>
+            import ('../components/create-expenses/CreateExpensesComponent'),
+    },
+    {
+        path: '/list-expenses',
+        name: 'List All Expenses',
+        component: () =>
+            import ('../components/list-expenses/ListExpensesComponent'),
+    },
+    {
+        path: '/edit-expenses/:id',
+        name: 'Update Expenses',
+        component: () =>
+            import ('../components/edit-expenses/EditExpensesComponent'),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
